e7eef36597bf3a89a09dd75429f939df54254e25
NULL-LI <lixiang3.1416@qq.com>
2018-07-25 14:06:51 +0800

    edit .gitignore

--- qt-build/CMakeFiles/rrt-test_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "rrt-test_automoc.cpp"
-  "CMakeFiles/rrt-test_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rrt-test_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
